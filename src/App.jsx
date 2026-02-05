3f5857ebc118dca7915fd6b0aee5fd086a3c4ebd
Kawagoe53 <y.kawagoe1231@gmail.com>
2026-02-05 22:49:46 +0900

    初回のコミット

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function PostsList({ src }) {
+  const formatDate = (createdAt) => {
+    const date = new Date(createdAt);
+    return date.toLocaleDateString("ja-JP");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="bg-white">
+      <header className="flex justify-between bg-[#333333] min-h-[5vh] items-center">
+        <p>Blog</p>
+        <p>問い合わせ</p>
+      </header>
 
-export default App
+      {src.map((posts) => (
+        <div key={posts.id} className="flex">
+          <img src={posts.thumbnailUrl} className="w-100px" />
+          <ul className="text-black">
+            <li>
+              {formatDate(posts.createdAt)},{posts.categories}
+            </li>
+            <li>{posts.title}</li>
+            <li>{posts.content}</li>
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
